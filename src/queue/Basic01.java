388be58d95be835baa1b6dfa8ceb669ac78e50da
doriver <tkdduq241@naver.com>
2024-03-15 21:46:46 +0900

    stack, queue

--- src/queue/Basic01.java
+++ src/queue/Basic01.java
@@ -6,18 +6,19 @@ import java.util.Queue;
 public class Basic01 {
 
 	public static void main(String[] args) {
-		Queue<String> queue = new LinkedList<>(); // Queue 는 인터페이스임
+		Queue<String> queue = new LinkedList<>(); // Queue는 인터페이스임, LinkedList로 생성
 		
+		// 데이터 추가 offer()
 		queue.offer("사과");
 		queue.offer("바나나");
 		queue.offer("체리");
 		System.out.println(queue); // [사과, 바나나, 체리]
 		
-		// 맨 앞의 요소 확인( 제거하지 않음 )
+		// 맨 앞의 요소 확인( 제거하지 않음 ) , peek()
 		String frontElement = queue.peek();
 		System.out.println(queue + "  " + frontElement); // [사과, 바나나, 체리]  사과
 		
-		// 요소 제거( 가장 처음 넣은거 )
+		// 요소 제거( 가장 처음 넣은거 ) , poll()
 		String removedElement = queue.poll();
 		System.out.println(queue + "  " + frontElement); // [바나나, 체리]  사과
 	}
--- src/stack/Basic01.java
+++ src/stack/Basic01.java
@@ -7,18 +7,32 @@ public class Basic01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Stack<String> stack = new Stack<>();
+		System.out.println(stack); // []
+		// stack 공간 비어있는지 확인 isEmpty()
+		System.out.println(stack.isEmpty()); // true
 		
-		// stack에 값넣기
+		
+		// stack에 데이터 추가 push()
 		stack.push("사과");
 		stack.push("바나나");
 		stack.push("체리");
 		System.out.println(stack); // [사과, 바나나, 체리]
+		System.out.println("    ====    ====   "); 
+		
+		// stack 요소 위치확인 search()
+		// pop() 메서드를 호출했을 때 몇 번째 순서로 나오는지에 대한 인덱스를 반환( 인덱스가 0이 아닌 1부터 시작 )
+		System.out.println(stack.search("사과")); // 3
+		System.out.println(stack.search("체리")); // 1
+		System.out.println(stack.search("22")); // -1(존재하지 않는경우)
+		
+		// stack 사이즈 size()
+		System.out.println(stack.size()); // 3
 
-		// 맨위의 요소 확인( 제거하는건 아님 )
+		// 맨위의 요소 확인( 제거하는건 아님 ) peek()
 		String topElement = stack.peek();
 		System.out.println(stack + "  " + topElement); // [사과, 바나나, 체리]  체리
 		
-		// 요소 제거( 가장 마지막에 넣은거 )
+		// stack에서 데이터 제거( 가장 마지막에 넣은거를 제거함 ) pop()
 		String poppedElement = stack.pop();
 		System.out.println(stack + "  " + poppedElement); // [사과, 바나나]  체리
 	}
