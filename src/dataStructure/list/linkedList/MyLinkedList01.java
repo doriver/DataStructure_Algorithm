f4104b01492d2708cfae233f2f7faa19c0ce158e
doriver <tkdduq241@naver.com>
2024-05-22 16:26:26 +0900

    LinkedList 구현 간단완성

--- src/dataStructure/list/linkedList/MyLinkedList01.java
+++ src/dataStructure/list/linkedList/MyLinkedList01.java
@@ -82,11 +82,53 @@ public class MyLinkedList01 {
 		current.next = current.next.next;
 	}
 	
+	// 특정 데이터를 가진 노드1개 삭제
+	public void deleteWithValue(int data) {
+		if (head == null) return;
+		
+		if (head.data == data) {
+			head = head.next;
+			return;
+		}
+		
+		Node01 current = head;
+		// 어려운듯
+		while (current.next != null) {
+			if (current.next.data == data) { // current.next 삭제하는경우
+				current.next = current.next.next;
+				return;
+			}
+			current = current.next; // current.next로 넘어가는 경우
+		}
+	}
 	
+	// 리스트 출력
+	public void printList() {
+		Node01 current = head;
+		while (current != null) {
+			System.out.print(current.data + " -> ");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyLinkedList01 list = new MyLinkedList01();
+		
+		list.append(1);
+		list.append(2);
+		list.append(3);
+		list.append(4);
+		
+		list.printList();
+		
+		list.deleteAtIndex(2);
+		System.out.println(list.get(2).data);
+		list.deleteWithValue(1);
+		list.prepend(10);
+		
+		list.printList();
 	}
 
 }
